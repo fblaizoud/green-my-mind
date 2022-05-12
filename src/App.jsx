1cce90eedfdae0d60c61258323db757559c7553d
Nicolas Vagnoux <nicolas.vagnoux@gmail.com>
2022-05-12 15:37:17 +0200

    Created homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.scss";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
 
 function App() {
-  return <div className="app" />;
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
