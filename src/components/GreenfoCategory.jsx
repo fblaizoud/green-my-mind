1d1397c21652767113cde05a3a0645d948f63dd4
fblaizoud <blaiz95@hotmail.com>
2022-05-12 22:26:21 +0200

    greenfodetail v2

--- src/components/GreenfoCategory.jsx
+++ src/components/GreenfoCategory.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import NavBarWithLink from "./NavBarWithLink";
 import { useParams } from "react-router-dom";
+import NavBarWithLink from "./NavBarWithLink";
 
 const GreenfoCategory = () => {
   const { title } = useParams();
