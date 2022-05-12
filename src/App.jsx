44a4a20acb4fc8d5b714440bd66a2fb610730daa
Nicolas Vagnoux <nicolas.vagnoux@gmail.com>
2022-05-12 17:49:43 +0200

    quiz 1st page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.scss";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import GreenfoPage from "./components/GreenfoPage";
+import QuizPage from "./components/QuizPage";
 
 function App() {
   return (
     <div className="app">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/greenfo" element={<GreenfoPage />} />
+        <Route path="/quiz" element={<QuizPage />} />
       </Routes>
     </div>
   );
--- src/components/QuizPage.jsx
+++ src/components/QuizPage.jsx
@@ -1,5 +1,14 @@
+import React from "react";
+
 const QuizPage = () => {
-  return <div />;
+  return (
+    <div className="quizPage">
+      <div className="quizPage__intro">
+        <p>Are you ready to assess your ecological footprint ?</p>
+        <button type="button">Start Quiz</button>
+      </div>
+    </div>
+  );
 };
 
 export default QuizPage;
