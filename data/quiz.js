9c054df1ef51caa9a62c435522a0597a036f7469
EstelleChardac <estelle.chardac@gmail.com>
2022-05-12 21:35:21 +0200

    quiz q&a

--- data/quiz.js
+++ data/quiz.js
@@ -1,289 +1,290 @@
 const quiz = [
   {
     id: 1,
-    question: "Ceci est la question 1",
+    question: "Which type of transports do you use every day?",
     answers: [
       {
         id: 1,
-        answer: "Ceci est la réponse 1",
+        answer: "Bicycle",
       },
       {
         id: 2,
-        answer: "Ceci est la réponse 2",
+        answer: "Public transports or carpool",
       },
       {
         id: 3,
-        answer: "Ceci est la réponse 3",
+        answer: "My fine Diesel car",
       },
     ],
   },
   {
     id: 2,
-    question: "Ceci est la question 2",
+    question: "How far is your home from your workplace?",
     answers: [
       {
         id: 1,
-        answer: "Ceci est la réponse 1",
+        answer: "I work from home",
       },
       {
         id: 2,
-        answer: "Ceci est la réponse 2",
+        answer: "0 - 20km",
       },
       {
         id: 3,
-        answer: "Ceci est la réponse 3",
+        answer: "20km+",
       },
     ],
   },
   {
     id: 3,
-    question: "Ceci est la question 3",
+    question: "How much distance do you travel per week?",
     answers: [
       {
         id: 1,
-        answer: "Ceci est la réponse 1",
+        answer: "10-20km",
       },
       {
         id: 2,
-        answer: "Ceci est la réponse 2",
+        answer: "20-50km",
       },
       {
         id: 3,
-        answer: "Ceci est la réponse 3",
+        answer: "50km+",
       },
     ],
   },
   {
     id: 4,
-    question: "Ceci est la question 4",
+    question: "How do you travel for long trips?",
     answers: [
       {
         id: 1,
-        answer: "Ceci est la réponse 1",
+        answer: "By train",
       },
       {
         id: 2,
-        answer: "Ceci est la réponse 2",
+        answer: "By car, I don't travel too far from home",
       },
       {
         id: 3,
-        answer: "Ceci est la réponse 3",
+        answer: "By plane",
       },
     ],
   },
   {
     id: 5,
-    question: "Ceci est la question 5",
+    question: "How often do you eat meat?",
     answers: [
       {
         id: 1,
-        answer: "Ceci est la réponse 1",
+        answer: "Never",
       },
       {
         id: 2,
-        answer: "Ceci est la réponse 2",
+        answer: "2-3 times/week",
       },
       {
         id: 3,
-        answer: "Ceci est la réponse 3",
+        answer: "Every day, dead or alive",
       },
     ],
   },
   {
     id: 6,
-    question: "Ceci est la question 6",
+    question: "How often do you eat organic food?",
     answers: [
       {
         id: 1,
-        answer: "Ceci est la réponse 1",
+        answer: "Every day",
       },
       {
         id: 2,
-        answer: "Ceci est la réponse 2",
+        answer: "Regularly",
       },
       {
         id: 3,
-        answer: "Ceci est la réponse 3",
+        answer: "Orga-what?",
       },
     ],
   },
   {
     id: 7,
-    question: "Ceci est la question 7",
+    question: "How do you drink your water?",
     answers: [
       {
         id: 1,
-        answer: "Ceci est la réponse 1",
+        answer: "On tap",
       },
       {
         id: 2,
-        answer: "Ceci est la réponse 2",
+        answer: "Bottled",
       },
       {
         id: 3,
-        answer: "Ceci est la réponse 3",
+        answer: "Both",
       },
     ],
   },
   {
     id: 8,
-    question: "Ceci est la question 8",
+    question:
+      "How much of the food you eat is unprocessed, unpackaged or locally grown?",
     answers: [
       {
         id: 1,
-        answer: "Ceci est la réponse 1",
+        answer: "All of it",
       },
       {
         id: 2,
-        answer: "Ceci est la réponse 2",
+        answer: "Some",
       },
       {
         id: 3,
-        answer: "Ceci est la réponse 3",
+        answer: "None",
       },
     ],
   },
   {
     id: 9,
-    question: "Ceci est la question 9",
+    question: "How do you heat your home?",
     answers: [
       {
         id: 1,
-        answer: "Ceci est la réponse 1",
+        answer: "Solar panels",
       },
       {
         id: 2,
-        answer: "Ceci est la réponse 2",
+        answer: "Gas",
       },
       {
         id: 3,
-        answer: "Ceci est la réponse 3",
+        answer: "Electricity",
       },
     ],
   },
   {
     id: 10,
-    question: "Ceci est la question 10",
+    question: "Do you generally shut your appliances down at home?",
     answers: [
       {
         id: 1,
-        answer: "Ceci est la réponse 1",
+        answer: "Yes",
       },
       {
         id: 2,
-        answer: "Ceci est la réponse 2",
+        answer: "No",
       },
       {
         id: 3,
-        answer: "Ceci est la réponse 3",
+        answer: "I can't, I'm mining bitcoins",
       },
     ],
   },
   {
     id: 11,
-    question: "Ceci est la question 11",
+    question: "Where do you dispose of your trash?",
     answers: [
       {
         id: 1,
-        answer: "Ceci est la réponse 1",
+        answer: "Compost/Plastic bin/Glass bin",
       },
       {
         id: 2,
-        answer: "Ceci est la réponse 2",
+        answer: "Glass and general waste",
       },
       {
         id: 3,
-        answer: "Ceci est la réponse 3",
+        answer: "I just leave my bags in the street",
       },
     ],
   },
   {
     id: 12,
-    question: "Ceci est la question 12",
+    question: "Which material is your house built with",
     answers: [
       {
         id: 1,
-        answer: "Ceci est la réponse 1",
+        answer: "Wood",
       },
       {
         id: 2,
-        answer: "Ceci est la réponse 2",
+        answer: "Steel",
       },
       {
         id: 3,
-        answer: "Ceci est la réponse 3",
+        answer: "Concrete",
       },
     ],
   },
   {
     id: 13,
-    question: "Ceci est la question 13",
+    question: "How often do you buy new clothes?",
     answers: [
       {
         id: 1,
-        answer: "Ceci est la réponse 1",
+        answer: "Hardly ever",
       },
       {
         id: 2,
-        answer: "Ceci est la réponse 2",
+        answer: "At least once a month",
       },
       {
         id: 3,
-        answer: "Ceci est la réponse 3",
+        answer: "As soon as they are worn out",
       },
     ],
   },
   {
     id: 14,
-    question: "Ceci est la question 14",
+    question: "How do you dispose of your worn out clothes?",
     answers: [
       {
         id: 1,
-        answer: "Ceci est la réponse 1",
+        answer: "I recycle them",
       },
       {
         id: 2,
-        answer: "Ceci est la réponse 2",
+        answer: "I give them away or sell them",
       },
       {
         id: 3,
-        answer: "Ceci est la réponse 3",
+        answer: "I throw them out",
       },
     ],
   },
   {
     id: 15,
-    question: "Ceci est la question 15",
+    question: "Where do you buy clothes?",
     answers: [
       {
         id: 1,
-        answer: "Ceci est la réponse 1",
+        answer: "Second hand stores",
       },
       {
         id: 2,
-        answer: "Ceci est la réponse 2",
+        answer: "Local shops",
       },
       {
         id: 3,
-        answer: "Ceci est la réponse 3",
+        answer: "High street",
       },
     ],
   },
   {
     id: 16,
-    question: "Ceci est la question 16",
+    question: "Which fabrics do you buy?",
     answers: [
       {
         id: 1,
-        answer: "Ceci est la réponse 1",
+        answer: "Only organic undyed cotton",
       },
       {
         id: 2,
-        answer: "Ceci est la réponse 2",
+        answer: "Warm wool for winter and nylon for spring",
       },
       {
         id: 3,
-        answer: "Ceci est la réponse 3",
+        answer: "Whatever feels good on my skin",
       },
     ],
   },
