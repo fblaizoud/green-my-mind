152ffb14fe61a5e008a7cf280235c1e95cf69977
EstelleChardac <estelle.chardac@gmail.com>
2022-05-12 23:11:54 +0200

    grid for categories

--- src/components/GreenfoCategory.jsx
+++ src/components/GreenfoCategory.jsx
@@ -10,6 +10,27 @@ const GreenfoCategory = () => {
       <NavBarWithLink />
       <div className={`greenfo_${title}`}>
         <h1>{title}</h1>
+
+        {title === "transport" && (
+          <div className="container">
+            <div className="text1">
+              A plane produces 300 times more carbon dioxyde than a train
+            </div>
+            <div className="graph1">300</div>
+            <div className="text2">
+              97% of transports depend on fossil energy
+            </div>
+            <div className="graph2"> 97% </div>
+            <div className="graph3">80% </div>
+            <div className="text3">
+              10yr+ old vehicles represent 80% of the worlds car pollution
+            </div>
+            <div className="graph4"> 1% </div>
+            <div className="text4">
+              Only 1% of vehicles in France are electric
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
