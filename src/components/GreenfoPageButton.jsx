6d32f7dcfcecd161ab57f7d45e4628c4a2efc33f
Nicolas Vagnoux <nicolas.vagnoux@gmail.com>
2022-05-12 21:18:26 +0200

    minor update on quiz and info

--- src/components/GreenfoPageButton.jsx
+++ src/components/GreenfoPageButton.jsx
@@ -5,9 +5,11 @@ import { Link } from "react-router-dom";
 const GreenfoPageButton = ({ title, image }) => {
   return (
     <Link to={`/greenfo/${title}`}>
-      <div className="greenfoButton">
-        <h2 className="greenfoButton__title">{title}</h2>
-        <img className="greenfoButton__img" src={image} alt="image_food" />
+      <div className="greenfoPreButton">
+        <div className="greenfoButton">
+          <h2 className="greenfoButton__title">{title}</h2>
+          <img className="greenfoButton__img" src={image} alt="image_food" />
+        </div>
       </div>
     </Link>
   );
