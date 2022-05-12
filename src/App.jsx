9a6ee5a727a41550912963d81b13d303d1bd4ca0
Vincent Lagarde <lagardevincent64@gmail.com>
2022-05-12 17:33:00 +0200

    add greenfopage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.scss";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import GreenfoPage from "./components/GreenfoPage";
 
 function App() {
   return (
     <div className="app">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/greenfo" element={<GreenfoPage />} />
       </Routes>
     </div>
   );
--- src/components/GreenfoPage.jsx
+++ src/components/GreenfoPage.jsx
@@ -1,5 +1,19 @@
+import GreenfoPageButton from "./GreenfoPageButton";
+
 const GreenfoPage = () => {
-  return <div />;
+  return (
+    <div className="greenfoPage">
+      <div className="greenfoPage__grid">
+        <GreenfoPageButton title="Food" image="./assets/imgFood.jpg" />
+        <GreenfoPageButton
+          title="Transport"
+          image="./assets/imgTransport.jpg"
+        />
+        <GreenfoPageButton title="Houses" image="./assets/imgHouse.jpg" />
+        <GreenfoPageButton title="Clothing" image="./assets/imgTextil.jpg" />
+      </div>
+    </div>
+  );
 };
 
 export default GreenfoPage;
